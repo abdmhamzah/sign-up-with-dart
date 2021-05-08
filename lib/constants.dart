f7b5b56b8667b630a09d0d1b8a4aceaa99deac68
abdmhamzah <iamhamzaham@gmail.com>
2021-05-09 06:23:10 +0700

    added button next in screen 1 and styling form email with added icon

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-TextStyle stepper =
-    TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.normal);
+TextStyle stepper = TextStyle(
+  color: Colors.black,
+  fontSize: 24,
+  fontWeight: FontWeight.normal,
+);
 
-TextStyle bodyBlack =
-    TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.normal);
+TextStyle bodyBlack = TextStyle(
+  color: Colors.black,
+  fontSize: 16,
+  fontWeight: FontWeight.normal,
+);
+
+TextStyle buttonText = TextStyle(
+  color: Colors.white,
+  fontSize: 17,
+  fontWeight: FontWeight.bold,
+);
--- lib/screens/screen 1/components/form_email.dart
+++ lib/screens/screen 1/components/form_email.dart
@@ -1,4 +1,6 @@
+import 'package:dkatalis/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:dkatalis/size_config.dart';
 
 class FormEmail extends StatelessWidget {
   @override
@@ -8,28 +10,42 @@ class FormEmail extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            padding: const EdgeInsets.only(
-              left: 15,
-              right: 15,
-              top: 15,
-              bottom: 15,
-            ),
+            padding: EdgeInsets.all(15),
             decoration: BoxDecoration(
               color: Colors.grey.shade100,
-              borderRadius: BorderRadius.circular(8),
+              borderRadius: BorderRadius.circular(10),
             ),
             child: Container(
-              padding: const EdgeInsets.only(left: 20, right: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
               decoration: BoxDecoration(
                 color: Colors.grey.shade300,
-                borderRadius: new BorderRadius.circular(10.0),
+                borderRadius: new BorderRadius.circular(8),
               ),
               child: TextFormField(
                 decoration: InputDecoration(
                   labelText: "Email",
                   border: InputBorder.none,
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  contentPadding: EdgeInsets.only(bottom: 15),
+                  icon: Icon(Icons.mail_outline),
                 ),
               ),
+            ),
+          ),
+          SizedBox(
+            height: 120,
+          ),
+          Container(
+            width: getProportionateScreenWidth(375),
+            child: TextButton(
+              onPressed: () {
+                print('press to submit');
+              },
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.blueAccent.shade100,
+                  padding: EdgeInsets.only(top: 15, bottom: 15),
+                  enableFeedback: true),
+              child: Text("Next", style: buttonText),
             ),
           )
         ],
